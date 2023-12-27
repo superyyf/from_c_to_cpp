674eec5a4847e62233c7d28cd2246ca053d7294f
YE Yunfei <yunfei.ye@cn.bosch.com>
2023-12-27 18:20:54 +0800

    update-12-27

--- cpp_demo.cpp
+++ cpp_demo.cpp
@@ -1,33 +1,18 @@
-#include <iostream>
-
-class Computer {
+class Mouse{
 public:
-  Computer(const char* _host_name, int _cpu_num);
-  ~Computer();
-  void print_info();
-
-private:
-  const char* host_name_;
-  int cpu_num_;
-  uint8_t *data_;
-
+  void click(){
+    /* do some thing */
+  }
 };
 
-Computer::Computer(const char* _host_name, int _cpu_num) : host_name_(_host_name), cpu_num_(_cpu_num) {
-  data_ = (uint8_t*)malloc(512);
-}
-
-Computer::~Computer(){
-  free(data_);
-}
-
-void Computer::print_info(){
-  std::cout << "Computer " << this->host_name_ << " has " << this->cpu_num_ << " cores." << std::endl;
-}
-
-int main(){
-  Computer zbook("zbook", 16);
-}
+class Zbook{
+  Mouse *m;
+  void play(){
+    if(m != nullptr){
+      m->click();
+    }
+  }
+};
 
 
 
