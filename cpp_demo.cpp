9d4be75216b98b45a1f78fdb346e45f07de09a5f
YE Yunfei <yunfei.ye@cn.bosch.com>
2024-01-04 18:03:37 +0800

    update-1-4

--- cpp_demo.cpp
+++ cpp_demo.cpp
@@ -1,19 +1,41 @@
+#include <iostream>
+#include <string>
+
 class Computer{
+private:
+  int screen;
+
+protected:
+  char* type;
+  
 public:
-  Mouse m;
-  virtual void play(){
-    m.click();
+  virtual void power_up(){
+    screen = 1;
+    type = "computer";
+    std::cout << std::string(type) << " power up with screen " << screen << std::endl;
   }
 };
 
 class Zbook : public Computer{
+private:
+  int camera;
+
 public:
-  KeyBoard k;
-  virtual void play(){
-      m.click();
-      k.hit();
+  virtual void power_up(){
+    // screen = 1;
+    camera = 1;
+    type = "notebook";
+    std::cout << std::string(type) << " power up with camera " << camera << std::endl;
   }
 };
 
-
-
+int main(){
+  Computer a;
+  // a.screen = 2;
+  // a.type = "iphone";
+  a.power_up();
+  Zbook z;
+  // z.camera = 2;
+  // z.type = "iphone";
+  z.power_up();
+}
